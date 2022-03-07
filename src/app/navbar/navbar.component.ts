398d0affd1b5c638845b7c83440db7ff9291b6d7
Nicolas Bayona <nbayonaord@gmail.com>
2022-03-07 11:59:49 -0500

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,28 +8,50 @@ declare let $: any;
 })
 export class NavbarComponent implements OnInit {
 
-  private static user: User|null=null;
   constructor() {}
 
-  static getUser(): String{
-    return (NavbarComponent.user==null)?"No user":NavbarComponent.user?.email;
-  }
-
   getUser():String{
-    return NavbarComponent.getUser();
+    let ret=NavbarComponent.getCookie("username");
+    return (ret.length>0)?ret:"No user";
   }
 
   logout():void{
+    NavbarComponent.createCookie("user", "");
+    NavbarComponent.createCookie("username", "");
+    NavbarComponent.createCookie("password", "");
     $("#logout").addClass("disabled");
-    NavbarComponent.user=null;
+  }
+
+  static getCookie(name: String): String{
+    let ret="";
+    let cname = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length && ret==""; i++){
+        let c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(cname) == 0){
+            ret=c.substring(cname.length, c.length);
+        }
+    }
+    return ret;
+  }
+
+  static createCookie(name: String, data: String):void{
+    let cookie = name+"="+data;
+    document.cookie = cookie;
   }
 
   ngOnInit(): void {    
   }
 
   static changeUser(user:User): String{
-    NavbarComponent.user=user;
+    this.createCookie("user", JSON.stringify(user));
+    this.createCookie("username", user.email);
+    this.createCookie("password", user.password);
     $("#logout").removeClass("disabled");
-    return NavbarComponent.getUser();
+    return this.getCookie("username");
   }
 }
