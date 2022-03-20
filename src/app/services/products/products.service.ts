ef27351ac5969cb48b99337243a08a2dc5690a13
Abril <abrilcanoc@gmail.com>
2022-03-20 14:55:41 -0500

    More Design

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -8,10 +8,10 @@ export class ProductsService {
   static products: Product[] = [];
 
   static poblate(){
-    ProductsService.products.push(new Product(1, 10.5, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/angular23_480x.jpg?v=1640032503", "NO", "Falta la descripcion", "offer"));
-    ProductsService.products.push(new Product(2, 12.2, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/Angular_360x.png?v=1614384169", "Aun no hay un nombre definido, estos e debe a que no hemos pensado en que clase de sacos vamos a vender, por ahora tenemos esto y ya. Ojala les guste, besos XoXo", "Texto sin sentido puesto pata quitarse en una proxima actualizacion  :V tengo la leve sospecha que esto lo traeremos de algun servicio web, por fVIOR alguien diga algo", "offer exclusive new"));
-    ProductsService.products.push(new Product(3, 19, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/Nodejs_360x.png?v=1614388512", "NO", "Falta la descripcion", "exclusive new"));
-    ProductsService.products.push(new Product(4, 18.2, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/AWS_M2_3b05ab76-7294-4974-9d97-77c35d96efa5_480x.jpg?v=1615480109", "NO", "Falta la descripcion", "exclusive"));
+    ProductsService.products.push(new Product(1, 10.5, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/angular23_480x.jpg?v=1640032503", "NOMBRE1", "Falta la descripcion", "offer"));
+    ProductsService.products.push(new Product(2, 12.2, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/Angular_360x.png?v=1614384169", "NOMBRE2", "Texto sin sentido puestoalgo", "offer exclusive new"));
+    ProductsService.products.push(new Product(3, 19, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/Nodejs_360x.png?v=1614388512", "NOMBRE3", "Falta la descripcion", "exclusive new"));
+    ProductsService.products.push(new Product(4, 18.2, "https://cdn.shopify.com/s/files/1/0537/9483/2552/products/AWS_M2_3b05ab76-7294-4974-9d97-77c35d96efa5_480x.jpg?v=1615480109", "NOMBRE4", "Falta la descripcion", "exclusive"));
   }
 
   static getProductById(idS: string): Product {
